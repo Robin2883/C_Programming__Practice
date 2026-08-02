3bcd1c74a2ed2f357107ceec755ca961d608a32b
Robin2883 <bluebillu83@gmail.com>
2026-08-02 14:31:32 +0600

    sum using pointer

---
+++ C Prac/pointer_sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a[]={6,8,13,23,67};
+    int sum=0, *ptr;
+    for (ptr=&a[0];ptr<=&a[4];ptr++)
+    {
+        sum+=*ptr;
+    }
+    printf("sum is %d\n", sum);
+    return 0;
+}
