1a134d602a06b61f12f6933717b222fedacc344d
Robin2883 <bluebillu83@gmail.com>
2026-09-14 17:48:22 +0600

    pop added to stack

--- C Prac/stack/syntax.c
+++ C Prac/stack/syntax.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int stack_arr[4];
 int top=-1;
@@ -13,11 +14,25 @@ void push (int data){
     printf("%d\n", stack_arr[top]);
 }
 
+int pop(){
+    int value;
+    if (top==-1){
+        printf("stack underflow");
+        exit(1); //abnormal termination of the program
+    }
+    value=stack_arr[top];
+    top=top-1;
+    return value;
+}
+
 int main(){
+    int data;
     push(1);
     push(2);
     push(3);
     push(4);
     //push(5); stack overflow
+    data=pop();
+    printf("%d\n", data);
     return 0;
 }
